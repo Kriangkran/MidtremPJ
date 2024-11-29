3ab56f0fbe880bb8ba94e0cadd191e569f6f7daf
Kriangkran <carabong6@gmail.com>
2024-11-29 17:31:10 +0700

    ben

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        BgBlue: "#EDF7FA",
+        secondry: "secondry"
+      }
+    },
   },
   plugins: [],
 }
